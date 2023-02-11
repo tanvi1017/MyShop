490cdc418cad964a07bfe5089eded6ca015512a9
tanvi1017 <tanavi@hipster-dev.com>
2023-02-11 13:12:39 +0530

    created drawable dimens.xml LoginActivity.kt RegistrationActivity.kt

--- app/src/main/java/com/tanvi/myshop/LoginActivity.kt
+++ app/src/main/java/com/tanvi/myshop/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.tanvi.myshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var tvRegister:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
+        tvRegister=findViewById(R.id.tvRegister)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        tvRegister.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
--- app/src/main/java/com/tanvi/myshop/SplashActivity.kt
+++ app/src/main/java/com/tanvi/myshop/SplashActivity.kt
@@ -11,11 +11,12 @@ import android.view.WindowManager
 import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
-    lateinit var tv_app_name:TextView
+    lateinit var tvMyShop:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        tvMyShop=findViewById(R.id.tvMyShop)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        tv_app_name=findViewById(R.id.tv_app_name)
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -27,11 +28,11 @@ class SplashActivity : AppCompatActivity() {
         }
         setContentView(R.layout.activity_main)
         Handler().postDelayed({
-            val i = Intent(this@SplashActivity,MainActivity::class.java)
+            val i = Intent(this@SplashActivity,LoginActivity::class.java)
             startActivity(i)
             finish()
         }, 3000)
         val typeface:Typeface =Typeface.createFromAsset(assets,"Kastore-Bold.otf")
-        tv_app_name.typeface =typeface
+        tvMyShop.typeface =typeface
     }
     }
